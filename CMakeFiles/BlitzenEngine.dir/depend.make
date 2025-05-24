3ed1168eaf7b3de95e6ec3b56e61af3225ff5194
PanosKappos2000 <pankappos00@gmail.com>
2025-05-24 13:49:07 +0300

    Got garbage inside the repo

--- CMakeFiles/BlitzenEngine.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for BlitzenEngine.
-# This may be replaced when dependencies are built.
--- CMakeFiles/copy_hlsl_shaders.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/copy_hlsl_shaders"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/copy_hlsl_shaders.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
